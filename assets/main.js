3bebe6e8a2437e6aa91df1e9a1f9a15484ec57ee
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-08-03 11:13:56 +0200

    Add nav bar styling and lang links

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,6 @@
+let navBar = document.querySelector(".nav-header");
 
+window.onscroll = () => {
+  if (window.scrollY > 300) navBar.classList.add("nav-active");
+  else navBar.classList.remove("nav-active");
+};
