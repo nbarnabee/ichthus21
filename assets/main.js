cba434c3f5feedeebc9f1958ccb4d6fd055743b2
Nicole Barnabee <nicole.barnabee@gmail.com>
2023-09-21 19:05:17 +0200

    Add obfuscation to email and phone number
    Client was concerned about web scraping

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,15 @@
 let navBar = document.querySelector(".nav-header");
+let phone = document.getElementById("obfus-phone");
+let emailAddress = document.getElementById("obfus-email");
 
 window.onscroll = () => {
-  if (window.scrollY > 150) navBar.classList.add("nav-active");
-  else navBar.classList.remove("nav-active");
+  if (window.scrollY > 150) {
+    navBar.classList.add("nav-active");
+    phone.textContent = "+41 (0) 79 193 92 03";
+    emailAddress.textContent = "contact@ichthus21.eu";
+  } else {
+    navBar.classList.remove("nav-active");
+    emailAddress.textContent = ".";
+    phone.textContent = ".";
+  }
 };
